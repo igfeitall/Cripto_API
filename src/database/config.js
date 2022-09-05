c2ed4267fae7c64dc6852799ada4534534d48fb9
igor feital <igfeitall@gmail.com>
2022-09-05 09:46:27 -0300

    adding log in configDB

--- src/database/config.js
+++ src/database/config.js
@@ -7,6 +7,15 @@ AWS.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 
+// AWS key logs
+AWS.config.getCredentials(function(err) {
+  if (err) console.log(err.stack); // credentials not loaded
+  else {
+    console.log("Access Key:", AWS.config.credentials.accessKeyId);
+    console.log("Secret Key:", AWS.config.credentials.secretAccessKey);
+  }
+})
+
 const dynamoClient = new AWS.DynamoDB.DocumentClient()
 const TABLE_NAME = 'token'
 
